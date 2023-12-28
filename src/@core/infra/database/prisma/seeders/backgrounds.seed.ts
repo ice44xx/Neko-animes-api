eb8e03ee05add03d1c4e379d63dcf2f4888f2a9b
Nathan Sant Helena <ice44xx@gmail.com>
2023-12-28 15:50:51 -0300

    fix: corrections in seeds

--- src/@core/infra/database/prisma/seeders/backgrounds.seed.ts
+++ src/@core/infra/database/prisma/seeders/backgrounds.seed.ts
@@ -3,40 +3,48 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function createBackgrounds() {
-  await prisma.backgrounds.create({
-    data: {
+  const backgroundsToAdd = [
+    {
+      id: 1,
       url: 'https://res.cloudinary.com/doupbxhfd/image/upload/v1690131720/Planos%20de%20fundo/cyber.webp',
       order: 1,
     },
-  });
-
-  await prisma.backgrounds.create({
-    data: {
+    {
+      id: 2,
       url: 'https://res.cloudinary.com/doupbxhfd/image/upload/v1690131720/Planos%20de%20fundo/demon.webp',
       order: 2,
     },
-  });
-
-  await prisma.backgrounds.create({
-    data: {
+    {
+      id: 3,
       url: 'https://res.cloudinary.com/doupbxhfd/image/upload/v1690131616/Planos%20de%20fundo/hashi.webp',
       order: 3,
     },
-  });
-
-  await prisma.backgrounds.create({
-    data: {
+    {
+      id: 4,
       url: 'https://res.cloudinary.com/doupbxhfd/image/upload/v1690131615/Planos%20de%20fundo/jujutsu.webp',
       order: 4,
     },
-  });
+  ];
 
-  console.log('Backgrounds geradas.');
+  for (const backgroundData of backgroundsToAdd) {
+    const existingBackground = await prisma.backgrounds.findUnique({
+      where: { id: backgroundData.id },
+    });
+
+    if (!existingBackground) {
+      await prisma.backgrounds.create({
+        data: backgroundData,
+      });
+    }
+  }
 }
 
 createBackgrounds()
-  .catch((e) => {
-    console.error(e);
+  .then(() => {
+    console.log('Backgrounds gerados com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao gerar backgrounds:', error);
   })
   .finally(async () => {
     await prisma.$disconnect();
--- src/@core/infra/database/prisma/seeders/classifications.seed.ts
+++ src/@core/infra/database/prisma/seeders/classifications.seed.ts
@@ -3,57 +3,63 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function createClassifications() {
-  await prisma.classifications.create({
-    data: {
+  const classificationsToAdd = [
+    {
+      id: 1,
       name: 'Shounen',
       desc: 'Energia, ação e aventura se fundem nesse gênero épico, ideal para quem busca histórias de superação, amizade e batalhas emocionantes.',
       thumbnail:
         'https://res.cloudinary.com/doupbxhfd/image/upload/v1703559298/Classifica%C3%A7%C3%B5es/Shounen_xfflya.webp',
     },
-  });
-
-  await prisma.classifications.create({
-    data: {
+    {
+      id: 2,
       name: 'Seinen',
       desc: 'Profundo e maduro, este gênero é uma jornada para o público adulto, explorando temas complexos, psicologia dos personagens e tramas intrincadas.',
       thumbnail:
         'https://res.cloudinary.com/doupbxhfd/image/upload/v1703559297/Classifica%C3%A7%C3%B5es/Seinen_zwjfhg.webp',
     },
-  });
-
-  await prisma.classifications.create({
-    data: {
+    {
+      id: 3,
       name: 'Shoujo',
       desc: 'Romance, emoção e uma dose saudável de drama! Feito para o público feminino, esse gênero encanta com histórias de amor, amizade e jornadas emocionantes.',
       thumbnail:
         'https://res.cloudinary.com/doupbxhfd/image/upload/v1703559298/Classifica%C3%A7%C3%B5es/Shoujo_hrvf00.webp',
     },
-  });
-
-  await prisma.classifications.create({
-    data: {
+    {
+      id: 4,
       name: 'Josei',
       desc: 'Maturidade e realismo definem esse gênero voltado para mulheres adultas. Com narrativas maduras sobre relacionamentos complexos e desafios da vida adulta.',
       thumbnail:
         'https://res.cloudinary.com/doupbxhfd/image/upload/v1703559297/Classifica%C3%A7%C3%B5es/Josei_ksnco4.webp',
     },
-  });
-
-  await prisma.classifications.create({
-    data: {
+    {
+      id: 5,
       name: 'Kodomo',
       desc: 'Cores vibrantes e lições divertidas! Projetado para as mentes jovens, este gênero cativante oferece histórias simples, educativas e cheias de diversão.',
       thumbnail:
         'https://res.cloudinary.com/doupbxhfd/image/upload/v1703559297/Classifica%C3%A7%C3%B5es/Kodomo_gm30ta.webp',
     },
-  });
+  ];
 
-  console.log('Classificações geradas.');
+  for (const classificationData of classificationsToAdd) {
+    const existingClassification = await prisma.classifications.findUnique({
+      where: { id: classificationData.id },
+    });
+
+    if (!existingClassification) {
+      await prisma.classifications.create({
+        data: classificationData,
+      });
+    }
+  }
 }
 
 createClassifications()
-  .catch((e) => {
-    console.error(e);
+  .then(() => {
+    console.log('Classificações geradas com sucesso.');
+  })
+  .catch((error) => {
+    console.error('Erro ao gerar classificações:', error);
   })
   .finally(async () => {
     await prisma.$disconnect();
