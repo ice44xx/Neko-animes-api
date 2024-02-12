da138d26df519e39b2ea13ef91ab189d241123e9
Nathan Sant Helena <ice44xx@gmail.com>
2024-02-12 04:48:08 -0300

    feat: removing seeds for production

--- src/main.ts
+++ src/main.ts
@@ -28,13 +28,11 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   });
 
-  if (envConfigService.getNodeEnv() === 'development') {
+  if (envConfigService.getNodeEnv() !== 'production') {
     const config = new DocumentBuilder().setTitle('Neko Animes').setDescription('Bem-vindo a Neko Animes Api').setVersion('1.0').build();
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
-
-    await generateSeeds();
   }
 
   await app.listen(3000);
